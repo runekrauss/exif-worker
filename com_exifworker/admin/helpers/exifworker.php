19771fd78fce1213e56e4a4c6148a2df2acf3950
Rune Krauss <5829946+runekrauss@users.noreply.github.com>
2015-03-23 10:34:11 +0100

    Program helpers for database queries

--- com_exifworker/admin/helpers/exifworker.php
+++ com_exifworker/admin/helpers/exifworker.php
@@ -1,1 +1,64 @@
 <?php
+
+// Disallow the direct calling of this file
+defined ( '_JEXEC' ) or die ();
+
+/**
+ * Helper for often used methods and access rights
+ * It will be performed by Controllers, Models...
+ *
+ * @author Rune Krauss
+ * @version 1.0
+ * @since 2015
+ * @copyright (c) by Rune Krauss
+ * @abstract
+ *
+ */
+abstract class ExifworkerHelper {
+	/**
+	 * Configure the Linkbar and get the actions
+	 *
+	 * @return Object Access rights
+	 * @static
+	 *
+	 * @access public
+	 */
+	public static function getActions($messageId = 0) {
+		$user = JFactory::getUser ();
+		$result = new JObject ();
+		
+		if (empty ( $messageId )) {
+			$assetName = 'com_exifworker';
+		} else {
+			$assetName = 'com_exifworker.message.' . ( int ) $messageId;
+		}
+		
+		$actions = array (
+				'core.admin',
+				'core.manage',
+				'core.create',
+				'core.edit',
+				'core.delete' 
+		);
+		
+		foreach ( $actions as $action ) {
+			$result->set ( $action, $user->authorise ( $action, $assetName ) );
+		}
+		
+		return $result;
+	}
+	
+	/**
+	 * Get timestamp "Y-m-d H:i:s"
+	 *
+	 * @return String Timestamp
+	 * @static
+	 *
+	 * @access private
+	 */
+	public static function getDateTime() {
+		$timestamp = time ();
+		$date = date ( "Y-m-d H:i:s", $timestamp );
+		return $date;
+	}
+}
--- com_exifworker/admin/tables/exifworker.php
+++ com_exifworker/admin/tables/exifworker.php
@@ -1,1 +1,57 @@
 <?php
+// Disallow the direct calling of this file
+defined ( '_JEXEC' ) or die ();
+
+// Import JTable class
+jimport ( 'joomla.database.table' );
+
+/**
+ * Table for database operations
+ *
+ * @author Rune Krauss
+ * @version 1.0
+ * @since 2015
+ * @copyright (c) by Rune Krauss
+ */
+class ExifworkerTableExifworker extends JTable {
+	/**
+	 * Constructor for this table
+	 *
+	 * @param JDatabase $db
+	 *        	JDatabase connector object
+	 * @access public
+	 */
+	public function __construct(&$db) {
+		parent::__construct ( '#__exifworker', 'ExifworkerId', $db );
+	}
+	
+	/**
+	 * Method to bind elements
+	 *
+	 * @return Object Elements
+	 * @access public
+	 */
+	public function bind($array, $ignore = '') {
+		if (isset ( $array ['params'] ) && is_array ( $array ['params'] )) {
+			// Convert the params field to a string.
+			$parameter = new JRegistry ();
+			$parameter->loadArray ( $array ['params'] );
+			$array ['params'] = ( string ) $parameter;
+		}
+		
+		return parent::bind ( $array, $ignore );
+	}
+	
+	/**
+	 * Method to compute the default name of the asset.
+	 * The default name is in the form `table_name.id`
+	 * where id is the value of the primary key of the table.
+	 *
+	 * @return string Asset
+	 */
+	protected function _getAssetName() {
+		$k = $this->_tbl_key;
+		
+		return 'com_exifworker.message.' . ( int ) $this->$k;
+	}
+}
